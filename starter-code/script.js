70f218b09350a0990c9b7e6176bf886158db8bf6
Guardianprime <okongordian@gmail.com>
2025-02-14 11:55:51 +0100

    password strength text automatically updating when checkbox clicked added

--- starter-code/script.js
+++ starter-code/script.js
@@ -2,15 +2,37 @@ const btn = document.getElementById("btn");
 const copyBtn = document.getElementById("copy-img");
 const copyText = document.getElementById("copy-text");
 const charLengthEl = document.getElementById("char-length");
+const strengthNameEl = document.getElementById("strength-name");
 const strengthStatusEl = document.querySelectorAll(".strength-status");
 const charLengthInput = document.getElementById("char-length-input");
 const passwordInputEl = document.getElementById("password-input");
-
+const checkboxes = document.querySelectorAll("#ul .checkbox");
 
 btn.addEventListener("click", generate);
 charLengthInput.addEventListener("change", (e) => {
     charLengthEl.textContent = e.target.value;
 });
+
+const passwordStrengthTexts = ["LOW", "MEDIUM", "HIGH", "VERY HIGH"];
+let passwordStrengthNumber = 0;
+
+checkboxes.forEach((checkbox) => {
+    checkbox.addEventListener("change", () => {
+        if (checkbox.checked) {
+            passwordStrengthNumber++;
+        } else {
+            passwordStrengthNumber--;
+        }
+        updatePasswordStrengthText();
+    });
+});
+
+function updatePasswordStrengthText() {
+
+    strengthNameEl.textContent = passwordStrengthTexts[passwordStrengthNumber];
+}
+
+updatePasswordStrengthText();
 
 let lowercase = "abscdefghijklmnopqrstuvwxyz";
 let uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -22,7 +44,7 @@ function generate() {
     const checkboxUppercase = document.getElementById("uppercase").checked;
     const checkboxNumbers = document.getElementById("numbers").checked;
     const checkboxSymbols = document.getElementById("symbols").checked;
-    charLength = parseInt(charLengthInput.value);
+    let charLength = parseInt(charLengthInput.value);
     const password = generatePassword(checkboxUppercase, checkboxNumbers, checkboxSymbols, charLength);
     passwordInputEl.value = password;
 }
